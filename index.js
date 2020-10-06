4c30c7938c109948af4e7f9f1bb3274b3541282d
Knyazevich <psknyazevich@gmail.com>
2020-10-07 00:31:50 +0300

    Remove unused function argument and variables
    Change all vars into consts;
    
    Add ending newline;

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
-var quotes = require("./data/data.json");
-var jokes = require("./data/jokes.json");
-var puns = require("./data/puns.json");
+const quotes = require("./data/data.json");
+const jokes = require("./data/jokes.json");
+const puns = require("./data/puns.json");
 
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-module.exports.getQuote = function (name) {
-  var index = randomInt(0, quotes.length - 1);
-  const quote = quotes[index];
-  
-  return quote;
+module.exports.getQuote = function() {
+  const index = randomInt(0, quotes.length - 1);
+  return quotes[index];
 };
 
 module.exports.getJoke = function() {
   const index = randomInt(0, jokes.length - 1);
-  const joke = jokes[index];
-
-  return joke;
+  return jokes[index];
 };
 
-module.exports.getPun = function () {
+module.exports.getPun = function() {
   const index = randomInt(0, puns.length - 1);
-  const pun = puns[index];
-
-  return pun;
+  return puns[index];
 };
